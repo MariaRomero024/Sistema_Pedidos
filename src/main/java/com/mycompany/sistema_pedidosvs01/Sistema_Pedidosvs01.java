b757b8323e68211465023a175c1377af18a51390
MariaRomero024 <mc309984@gmail.com>
2025-07-06 14:58:46 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/mycompany/sistema_pedidosvs01/Sistema_Pedidosvs01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_pedidosvs01;
+
+/**
+ *
+ * @author Maria
+ */
+public class Sistema_Pedidosvs01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
